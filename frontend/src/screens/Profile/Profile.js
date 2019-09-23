91628732ccf1f095f0c491ed19a53b0998fcee26
Vukknesh <leomcn@hotmail.com>
2019-09-22 22:47:43 -0300

    'go'

--- frontend/src/screens/Profile/Profile.js
+++ frontend/src/screens/Profile/Profile.js
@@ -1,19 +1,27 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
-export default function Profile() {
-  const auth = useSelector(state => state.auth);
-  const dispatch = useDispatch();
 
+const mapState = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default function Profile(props) {
+  const auth = useSelector(state => state.auth);
+  console.log(auth.isAuthenticated);
+  const { isAuthenticated } = useSelector(mapState);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!isAuthenticated) {
+      props.history.push("/");
+    }
+  }, [isAuthenticated]);
+  console.log(auth);
   return (
     <div>
-      {!auth.isAuthenticated ? (
-        <Redirect to="/" />
-      ) : (
-        <button onClick={() => dispatch({ type: "LOGOUT_SUCCESS" })}>
-          Logout
-        </button>
-      )}
+      <button onClick={() => dispatch({ type: "LOGOUT_SUCCESS" })}>
+        Logout
+      </button>
     </div>
   );
 }
